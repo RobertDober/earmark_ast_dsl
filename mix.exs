1a38c5dcd31600c9033ba67a60bba32589a0ac06
RobertDober <robert.dober@gmail.com>
2020-06-15 16:57:36 +0200

    Xtracted README

--- mix.exs
+++ mix.exs
@@ -56,7 +56,9 @@ defmodule EarmarkAstDsl.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 1.0.0"}
+      {:dialyxir, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:extractly, "~>0.1.5", only: [:dev]}
+
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
