b37788bd27e5b5795d13027ddf580a102df3e476
RobertDober <robert.dober@gmail.com>
2021-12-28 09:18:14 +0100

    RELEASE v0.3.4 prepared in mix.exs

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
-defmodule EarmarkAstDsl.MixProject do
+    defmodule EarmarkAstDsl.MixProject do
   use Mix.Project
 
-  @version "0.3.3"
+  @version "0.3.4"
   @url "https://github.com/robertdober/earmark_ast_dsl"
 
   @description """
